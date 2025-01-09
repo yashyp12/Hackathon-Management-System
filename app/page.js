e4a254477b7f9738343b9d6fb43566abd38ab701
Yash Patil <patilyashyp12@gmail.com>
2025-01-09 22:15:45 +0530

    remove unused import of Hero component from page.js

--- app/page.js
+++ app/page.js
@@ -1,8 +1,7 @@
 "use client";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import Hero from "@/components/hero";
-
+ 
 export default function Home() {
   const router = useRouter();
 
